7f756f0191b4f541fe928b8868fed300bc1aac0d
Allen Chou <chouallen09@gmail.com>
2023-10-11 19:49:03 -0700

    todo app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import React from 'react';
+
+
+
+
+
+
+function App() {
+  return (
+    <div className='todo-app'>
+      <TodoList />
+
+
+    </div>
+  );
+}
+
+export default App;
